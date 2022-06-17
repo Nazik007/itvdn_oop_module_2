357ef0f0cf66f9f6cd2145cd1ca6c7636a53f113
Nazarii Soviak <99899134+Nazik007@users.noreply.github.com>
2022-06-17 18:53:54 +0300

    Add files via upload

---
+++ itvdn_classes_objects_5/Main.java
@@ -0,0 +1,7 @@
+package itvdn_classes_objects_5;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println(MyArea.areaOfCircle(9));
+    }
+}
---
+++ itvdn_classes_objects_5/MyArea.java
@@ -0,0 +1,11 @@
+package itvdn_classes_objects_5;
+
+
+public class MyArea {
+    private static final double PI_NUMBER = 3.14F;
+
+     public static double areaOfCircle (double r) {
+         return  PI_NUMBER * r * r;
+    }
+}
+
